2192391e09e29e0c7c576e4ca02b884bc36b4184
tjrelly <xxterrelljacksonxx@gmail.com>
2024-05-16 08:52:59 -0400

    part 1 complete

--- models.py
+++ models.py
@@ -12,7 +12,15 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
-class User():
+class User(db.Model):
     """Site user"""
     
     __tablename__ = "users"
+
+    username = db.Column(db.String(20), primary_key=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    
+    
